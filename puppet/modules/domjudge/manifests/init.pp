f39f32e1750256a469fdf32893d7105ffaee62d6
Alex Muntada <alexm@alexm.org>
2014-08-05 23:59:28 +0200

    enable SSL in domjudge database

--- puppet/modules/domjudge/manifests/init.pp
+++ puppet/modules/domjudge/manifests/init.pp
@@ -1,5 +1,6 @@
 class domjudge {
   include apt
+  include stdlib
 
   if $::osfamily != 'Debian' {
     fail("Unsupported osfamily: ${::osfamily}, module domjudge only supports osfamily Debian.")
@@ -15,5 +16,24 @@ class domjudge {
 
   Package {
     require => Apt::Source['domjudge']
+  }
+
+  package { 'domjudge-common':
+    ensure => present,
+  }
+
+  File_line {
+    path    => '/etc/domjudge/common-config.php',
+    require => Package['domjudge-common'],
+  }
+
+  file_line {
+    'common-config.php flags null':
+      line  => "// define('DJ_MYSQL_CONNECT_FLAGS', null);",
+      match => "define\\('DJ_MYSQL_CONNECT_FLAGS', null\\);";
+
+    'common-config.php flags SSL':
+      line  => "define('DJ_MYSQL_CONNECT_FLAGS', MYSQLI_CLIENT_SSL);",
+      match => "define\\('DJ_MYSQL_CONNECT_FLAGS', MYSQLI_CLIENT_SSL\\);";
   }
 }
